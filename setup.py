adc50d9739724364e89bed66e5d42ae3df006984
sarthak-nference <166585429+sarthak-nference@users.noreply.github.com>
2024-08-16 10:26:41 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="ifCNV",
+    version=__version__,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.21',
+        'pandas>=1.3',
+        'scikit-learn>=1.0.1',
+        'plotly>=5.4',
+        "pybedtools>=0.8.2"
+    ]
+)
